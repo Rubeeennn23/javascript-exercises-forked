6717f0f14f4a6a900df32f949d099cb273d181c0
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-06-30 17:53:12 +0200

    exercises one and two done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
-
+function repeatString(word,times) {
+    let string='';
+    if (times<0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+// npm test repeatString.spec.js
 // Do not edit below this line
 module.exports = repeatString;
