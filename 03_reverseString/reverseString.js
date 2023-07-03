d53c06e328001b26375783062d32429decfa22f9
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-07-03 18:42:04 +0200

    done exercise 3 and started 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let length = words.length; 
+    let reverseWords = ''; 
+    for (let i=length-1;i>=0;i--) {
+        reverseWords += words[i];
+    } return reverseWords
 };
-
+// npm test reverseString.spec.js
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,object1,object2) {
+    let length = array.length;
+    for (let i=0; i<=length-1; i++) {
+        if (object1===array[i]) {
+            let newArray=array.splice(i,1)
+        } else {
+            newArray=array
+        
+             } if (object2===array[i]) {
+            let newArray=array.splice(i,1)
+        } else {
+            newArray=array
+        }
+    } return newArray
 };
 
+// npm test removeFromArray.spec.js
 // Do not edit below this line
 module.exports = removeFromArray;
