f1e743cd0fbf68d5ed82dca356a8199a2445c7f7
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-07-06 14:51:32 +0200

    completed test leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(num) {
+    if (num%4==0 && num%100!==0) {
+        return true
+    } else if (num%100==0 && num%400==0){
+        return true
+    } else {
+        return false
+    }
+}; 
+// npm test leapYears.spec.js
 // Do not edit below this line
 module.exports = leapYears;
