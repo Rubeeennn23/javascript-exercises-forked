e4195729bc600c2ff1bc5987549106da897a9c4d
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-07-06 12:34:22 +0200

    ended the test using rest parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,19 @@
-const removeFromArray = function(array,object1,object2) {
+const removeFromArray = function(array,... manyArgs) {
     let length = array.length;
-    for (let i=0; i<=length-1; i++) {
-        if (object1===array[i]) {
-            let newArray=array.splice(i,1)
-        } else {
-            newArray=array
-        
-             } if (object2===array[i]) {
-            let newArray=array.splice(i,1)
-        } else {
-            newArray=array
+    for (const arg of manyArgs) {
+        for (let i=0; i<=length-1; i++) {
+            if (array[i] === arg) {
+                let newArray=array.splice(i,1)
+            } else {
+                newArray=array
+            }
         }
     } return newArray
 };
+        
+        
 
+//removeFromArray([1,2,3,4],2,3);
 // npm test removeFromArray.spec.js
 // Do not edit below this line
 module.exports = removeFromArray;
