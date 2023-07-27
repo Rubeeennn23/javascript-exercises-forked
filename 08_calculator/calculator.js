d9c4668364cfd64a3f9b76efc78edcfdef96f424
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-07-27 17:38:56 +0200

    created the doc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,14 +1,28 @@
-const add = function() {
-	
+const add = function(a,b) {
+	if (a == undefined || b==undefined) {
+    return 0
+  } else {
+  return a+b
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let suma = Number('0')
+	for (let arg in args) {
+    if (arg !== undefined) {
+    suma = suma + Number(arg)
+} return suma
+  }
 };
+
+/*  } else if(a==undefined) {
+    return b
+  } else if(b==undefined) {
+    return a*/
 
 const multiply = function() {
 
@@ -22,6 +36,7 @@ const factorial = function() {
 	
 };
 
+// npm test calculator.spec.js
 // Do not edit below this line
 module.exports = {
   add,
