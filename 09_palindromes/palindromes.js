d6cf5e0a5e8bbf4dfb7c335fb37743b61d32472e
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-07-31 13:11:41 +0200

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let oldtext = string
+    let replacedText = oldtext.replace(/[^\w\s\']|_/g,'').replace(/\s+/g,'')
+    let text = replacedText.toLowerCase()
+    let length = text.length
+    let newText = ''
+    
+    for (let i=length; i>0; i--) {
+        newText += text.slice(i-1,i)
+    } if (newText == text) {
+        return true
+    } else {
+        return false
+    }
 };
-
+// npm test palindromes.spec.js
 // Do not edit below this line
 module.exports = palindromes;
