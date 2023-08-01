563e19a9b7ce49d6b720a31d3e18d5531d6ce702
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-08-01 11:59:59 +0200

    task solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    let sequence = [0,1]
+    let num = Number(arg)
+    if (num<0) {
+        return "OOPS"
+    } else {
+        for (let i=2; i<=num; i++) {
+            sequence[i]=sequence[i-2]+sequence[i-1]
+        } 
+    } return sequence[num]
 
 };
-
+// npm test fibonacci.spec.js
 // Do not edit below this line
 module.exports = fibonacci;
