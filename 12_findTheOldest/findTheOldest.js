8b6bb3cff6c8157689411719bb80c88d97cb697b
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-08-03 17:18:21 +0200

    created task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age = people.map(person => person.yearOfDeath - person.yearOfBirth)
+    return result
 
 };
-
+// npm test findTheOldest.spec.js
 // Do not edit below this line
 module.exports = findTheOldest;
