6314db8f4da0b5bfa82f154d7df8b8be13f7ab9c
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-08-03 16:58:39 +0200

    solved task 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    return books.map(item => item.title); 
+
 
 };
-
+// npm test getTheTitles.spec.js
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+/*
+    let length = books.length
+    let titles = []
+    for (let i=0; i<=length; i++) {
+        titles[i] = books[i].title
+    } return titles
+
+*/
