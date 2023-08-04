4f1d04b0d3f3ff4c8a715fd8adbae7da9d215503
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-08-04 10:29:31 +0200

    ended the last task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
 const findTheOldest = function(people) {
-    return people.sort()
+    return people.reduce((oldest,currentPerson) => {
+        const oldestAge = defineAge(oldest.yearOfBirth,oldest.yearOfDeath);
+        const currentAge = defineAge(currentPerson.yearOfBirth,currentPerson.yearOfDeath);
+        return oldestAge > currentAge ? oldest : currentPerson;
+    });
+    
+    function defineAge(yearOfBirth,yearOfDeath) {
+        if (!yearOfDeath) {
+            yearOfDeath = new Date().getFullYear()
+        } return yearOfDeath-yearOfBirth
+    }
      
 };
 // npm test findTheOldest.spec.js
